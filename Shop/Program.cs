3a19720a8764ba5ee092f1828ee6fa78403db172
Роман Карнаухов <WorkKarn@yandex.ru>
2024-04-20 11:38:48 +0700

    Добавьте файлы проекта.

---
+++ Shop/Program.cs
@@ -0,0 +1,12 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
